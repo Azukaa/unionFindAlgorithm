88ff94386ef58d6610c8221e7792c814c28b6c2c
Azukaa <34044355+Azukaa@users.noreply.github.com>
2020-03-29 12:18:54 +0100

    Create weightedQuickUnion.py

---
+++ weightedQuickUnion.py
@@ -0,0 +1,32 @@
+class WeightedQuickUnion:
+    def __init__(self, n):
+        self.n = int(n)
+        self.objects = [obj for obj in range(0, self.n)]
+        self.size = [obj for obj in range(0, self.n)]
+
+
+    def __iter__(self):
+        return iter(self.objects)
+
+    def root(self, i):
+        while i != self.objects[i]:
+            i = self.objects[self.objects[i]]
+
+        return i
+
+    def connected(self, p, q):
+        return self.root(p) == self.root(q)
+
+
+    def union(self, p, q):
+        pid = self.root(p)
+        qid = self.root(q)
+        
+        if self.size[pid] < self.size[qid]:
+            self.objects[pid] = qid
+            self.size[qid] += self.size[pid]
+        else:
+            self.objects[qid] = pid
+            self.size[pid] += self.size[qid]
+
+        return self.objects
